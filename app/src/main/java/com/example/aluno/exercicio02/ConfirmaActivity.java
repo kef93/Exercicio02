80cb2ded7f1ddbcaa933ab865d2917e069468591
fabio <koerich8@gmail.com>
2019-09-17 21:48:29 -0300

    update da Activity2

--- app/src/main/java/com/example/aluno/exercicio02/ConfirmaActivity.java
+++ app/src/main/java/com/example/aluno/exercicio02/ConfirmaActivity.java
@@ -1,7 +1,11 @@
 package com.example.aluno.exercicio02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ConfirmaActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class ConfirmaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirma);
+        Intent intent = getIntent();
+        //Chamando o bundle da MainActivity
+        Bundle bundle = intent.getExtras();
+
+
+
+        String nome = (String) intent.getSerializableExtra("nome");
+        String date = (String) intent.getSerializableExtra("date");
+        String email = (String) intent.getSerializableExtra("email");
+        String renda = (String) intent.getSerializableExtra("renda");
+        String estadocivil = (String) intent.getSerializableExtra("estado civil");
+        String sexo = (String) intent.getSerializableExtra("sexo");
+
+        TextView txvNome = (TextView) findViewById(R.id.txvNome);
+        TextView txvDate = (TextView) findViewById(R.id.txvDate);
+        TextView txvEmail = (TextView) findViewById(R.id.txvEmail);
+        TextView txvRenda = (TextView) findViewById(R.id.txvRenda);
+        TextView txvEstadocivil = (TextView) findViewById(R.id.txvEstadocivil);
+        TextView txvSexo = (TextView) findViewById(R.id.txvSexo);
+
+        Intent intent1 = getIntent();
+
+
+        txvNome.setText("Nome: "+ bundle.getString("nome"));
+        txvDate.setText("Data de Nascimento: "+ bundle.getString("date"));
+        txvEmail.setText("Email: "+ bundle.getString("email"));
+        txvRenda.setText("Renda:R$ "+ bundle.getString("renda"));
+        txvEstadocivil.setText("Estado Civil: "+ bundle.getString("estadocivil"));
+        txvSexo.setText("Sexo: "+ bundle.getString("sexo"));
     }
 }
--- app/src/main/java/com/example/aluno/exercicio02/MainActivity.java
+++ app/src/main/java/com/example/aluno/exercicio02/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.aluno.exercicio02;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +22,7 @@ import java.nio.channels.SeekableByteChannel;
 public class MainActivity extends AppCompatActivity {
 
     TextView mostrarenda;
+    String sexo = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +31,18 @@ public class MainActivity extends AppCompatActivity {
         final EditText nome = (EditText) findViewById(R.id.txtNome);
         final EditText date = (EditText) findViewById(R.id.txtData);
         final EditText email = (EditText) findViewById(R.id.txtEmail);
-        SeekBar seekrenda = (SeekBar) findViewById(R.id.seekRenda);
+        final SeekBar seekrenda = (SeekBar) findViewById(R.id.seekRenda);
         final Spinner estadocivil = (Spinner) findViewById(R.id.spnEstado);
         final RadioButton radbtnmasc = (RadioButton) findViewById(R.id.radbtnMasc);
         final RadioButton radbtnfem = (RadioButton) findViewById(R.id.radbtnFem);
         mostrarenda = (TextView) findViewById(R.id.txvValorRenda);
+        Button btnConfere = (Button) findViewById(R.id.btnConfere);
+        Button btnConfirma = (Button) findViewById(R.id.btnConfirma);
 
-        //Criando a barra de renda pulando de 50 em 50 até 20K
+
+
+
+         //Criando a barra de renda pulando de 50 em 50 até 20K
         SeekBar.OnSeekBarChangeListener renda = new SeekBar.OnSeekBarChangeListener() {
             //método para quando o Usuário muda a barra de lugar
             @Override
@@ -54,18 +62,28 @@ public class MainActivity extends AppCompatActivity {
         };
         seekrenda.setOnSeekBarChangeListener(renda);
 
-        //Botões Masculino e Feminino
-        Button ok = (Button) findViewById(R.id.btnConfere);
-        ok.setOnClickListener(new View.OnClickListener() {
+        //Criando Botão Confere
+        btnConfere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            //Botões Masculino e Feminino e um output para cada sexo selecionado
             if(radbtnmasc.isChecked()== true) {
-                Toast.makeText(getBaseContext(), "Masculino selecionado",
-                        Toast.LENGTH_SHORT).show();
-            }else if (radbtnfem.isChecked()== true){
-                Toast.makeText(getBaseContext(),"Feminino selecionado",
-                        Toast.LENGTH_SHORT).show();
-                }
+                sexo = "Masculino";
+                }else if (radbtnfem.isChecked()== true){
+                    sexo = "Feminino";
+            }
+            //Criando Alerta para todas as variáveis
+            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setMessage(
+                        "Nome: " + nome.getText().toString()
+                                +"\nNascimento: " + date.getText().toString()
+                                +"\nEmail: " + email.getText().toString()
+                                +"\nRenda: R$" + seekrenda.getProgress()
+                                +"\nEstado Civil: " +  estadocivil.getSelectedItem().toString()
+                                +"\nSexo: " + sexo
+                );
+            dlg.setNeutralButton("Ok", null);
+            dlg.show();
             }
         });
         //Criando Spinner
@@ -74,16 +92,26 @@ public class MainActivity extends AppCompatActivity {
                         R.layout.support_simple_spinner_dropdown_item);
         estadocivil.setAdapter(adapter);
 
-        ok.setOnClickListener(new View.OnClickListener() {
+        //Criado botão Confirma
+        btnConfirma.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String item = estadocivil.getSelectedItem().toString();
-                int posicao = estadocivil.getSelectedItemPosition();
-                Toast.makeText(getBaseContext(), "O item escolhido foi " + item +
-                                ", que está na posição:" + posicao,
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                //Criando intent no botão Confirma
+                Intent intent = new Intent(MainActivity.this, ConfirmaActivity.class);
+                //Criando Bundle para agrupar todas as variáveis no botão Confirma
+                Bundle bundle = new Bundle();
+
+                bundle.putString("nome", nome.getText().toString());
+                bundle.putString("date", date.getText().toString());
+                bundle.putString("email", email.getText().toString());
+                bundle.putString("renda", Integer.toString(seekrenda.getProgress()));
+                bundle.putString("estadocivil", estadocivil.getSelectedItem().toString());
+                bundle.putString("sexo", sexo);
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+
             }
         });
-
     }
 }
